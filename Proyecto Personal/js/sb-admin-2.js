31747ae20d2a69280bbb102940c74569f17c7555
WilliamFernando97 <70359210+WilliamFernando97@users.noreply.github.com>
2023-02-11 10:00:36 -0500

    Update sb-admin-2.js

--- Proyecto Personal/js/sb-admin-2.js
+++ Proyecto Personal/js/sb-admin-2.js
@@ -65,7 +65,7 @@ let mainContent = document.querySelector('#main_content');
 let secondaryContent = document.querySelector('#secondary_content');
 
 document.querySelector('.switch input').addEventListener('change', e => { 
-  console.log("Primer Boton");
+  
   mainContent.style.display = e.target.checked ? 'block' : 'none';
   secondaryContent.style.display = e.target.checked ? 'none' : 'block';
 });
